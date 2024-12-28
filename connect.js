72b9a09ac6915936ac15eb47a04f7682f6dc16ff
therealco <hawkinjames573@gmail.com>
2024-12-28 18:08:57 -0500

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,2 +1,4 @@
 console.log("Connecting to server...")
-const run = fetch(https://github.com/therealco/powerpy
+const run = fetch("https://raw.githubusercontent.com/therealco/powerpy/refs/heads/main/compile-server.sh")
+console.log("Run this code in Bash:")
+console.log(run)
